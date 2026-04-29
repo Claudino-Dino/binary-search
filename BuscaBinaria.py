143e772e09d8fc9c89d4a85d95129759e6eaf3bd
Dino <138309491+Claudino-Dino@users.noreply.github.com>
2026-04-29 15:07:07 -0300

    Update BuscaBinaria.py

--- BuscaBinaria.py
+++ BuscaBinaria.py
@@ -15,6 +15,7 @@ def busca_binaria(vetor, alvo, min, max):
     print("Elemento não existe no vetor!")
     return -1 
 
+# Encolha esse vetor clicando na seta para cima ^
 vetor_teste = [1000,
 2000,
 3000,
